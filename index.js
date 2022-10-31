3b5fe04ad2ad99aef981316d3fc2ab45da5fe7ed
gorgbus <63368174+gorgbus@users.noreply.github.com>
2022-10-31 21:30:54 +0100

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const core = require("@actions/core");
+
+(
+    async () => {
+        try {
+            const gistId = core.getInput("gist_id");
+
+            const res = await fetch(`https://api.github.com/gists/${gistId}`);
+
+            if (res.status !== 200) throw res.statusText;
+
+            const data = await res.json();
+
+            if (!data.files) throw "No files were found";
+
+            const fileName = core.getInput("file_name");
+            const files = Object.values(data.files);
+            let file;
+        
+            if (!fileName) file = files[0];
+            else if (files.some(file => file.filename === fileName)) file = files.find(file => file === fileName);
+            else throw "No file matched the provided name";
+            
+            core.setOutput("file", file.content);
+        } catch (err) {
+            core.setFailed(err.message);
+        }
+    }
+)();
